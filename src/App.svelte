4e5486d537f9f711615d167207809de5cec720d7
Chun Yue Leung <aryue123321@gmail.com>
2022-05-17 06:26:07 -0700

    more styling

--- src/App.svelte
+++ src/App.svelte
@@ -3,28 +3,37 @@
 	import Settings from "./components/Settings.svelte";
 	let openSettings = false;
 </script>
-
-<main>
-	<div class="container">
-		<div class="columns is-centered">
-			<div class="column is-half">
-				<Board>
-					<svelte:fragment slot="setting-button">
-						<div>
-							<button 
-								on:click="{()=>openSettings = true}"
-								class="button is-info"
-								>Settings
-							</button>
-						</div>
-					</svelte:fragment>
-				</Board>
+<div class="outer">
+	<main class="main">
+		<div class="container">
+			<div class="columns is-centered">
+				<div class="column is-half">
+					<Board>
+						<svelte:fragment slot="setting-button">
+							<div>
+								<button 
+									on:click="{()=>openSettings = true}"
+									class="button is-info"
+									>Settings
+								</button>
+							</div>
+						</svelte:fragment>
+					</Board>
+				</div>
 			</div>
 		</div>
-	</div>
-	
-</main>
-<Settings on:close={()=>openSettings = false} show={openSettings}/>
-<style>
+	</main>
+	<Settings on:close={()=>openSettings = false} show={openSettings}/>
+</div>
 
+<style>
+	.outer{
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
+	.main{
+		margin-top: auto;
+		margin-bottom: auto;
+	}
 </style>
